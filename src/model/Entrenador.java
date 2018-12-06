7b9f88f34ee4bcdd9d7e64bbccfde92e2449cde6
i32arroj <jm.aranda@uma.com>
2018-12-06 12:40:08 +0100

    Añadido jugador,equipo,entrenador y arbitro a model

---
+++ src/model/Entrenador.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author josep
+ */
+public class Entrenador extends Persona{
+    char nivel;
+    int id_partido;
+    
+    
+}
---
+++ src/model/Jugador.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+/**
+ *
+ * @author josep
+ */
+public class Jugador extends Persona{
+    double altura,peso,id_equipo;
+    int dorsal;
+
+    
+    
+}
